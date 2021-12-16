f00669f1e5a0186b77cca8dfac9b70935b2dc290
Leandro Alves <leandrosustenido@gmail.com>
2021-12-16 02:42:25 -0300

    :sparkles: add create multiple services when attendee is created

--- src/api/attendee/services/attendee.js
+++ src/api/attendee/services/attendee.js
@@ -6,4 +6,25 @@
 
 const { createCoreService } = require('@strapi/strapi').factories;
 
-module.exports = createCoreService('api::attendee.attendee');
+module.exports = createCoreService('api::attendee.attendee', ({ strapi }) => ({
+  async create(params) {
+
+    const result = await super.create(params);
+
+    const lectures = await strapi.db.query('api::lecture.lecture').findMany()
+
+    console.log({ result })
+
+    lectures.map((lecture) => {
+      strapi.db.query('api::service.service').create({
+        data: {
+          present: false,
+          lecture: lecture.id,
+          attendee: result.id
+        }
+      })
+    })
+
+    return result;
+  }
+}));
