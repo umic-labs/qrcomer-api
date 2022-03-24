9b29d0458ee502008a297093ad6d0d2c6f9a5e1e
Leandro Alves <leandrosustenido@gmail.com>
2022-03-23 21:13:01 -0300

    :bug: fix lecture and meal verbose serialization

--- src/api/lecture/controllers/lecture.js
+++ src/api/lecture/controllers/lecture.js
@@ -6,4 +6,11 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::lecture.lecture');
+module.exports = createCoreController('api::lecture.lecture', ({ strapi }) =>  ({
+  async find(ctx) {
+    const query = { ...ctx.query }
+    const services = await strapi.service('api::lecture.lecture').find(query);
+
+    return services.results;
+  },
+}))
--- src/api/meal/controllers/meal.js
+++ src/api/meal/controllers/meal.js
@@ -6,4 +6,11 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::meal.meal');
+module.exports = createCoreController('api::meal.meal', ({ strapi }) =>  ({
+  async find(ctx) {
+    const query = { ...ctx.query }
+    const services = await strapi.service('api::meal.meal').find(query);
+
+    return services.results;
+  },
+}))
