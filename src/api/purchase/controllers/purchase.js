ce9b490b4ba1e041b661183de5ae2a2a51ac7f78
Leandro Alves <leandrosustenido@gmail.com>
2022-11-21 01:23:29 -0300

    feat: Adiciona Attendees à resposta de criação de Purchase (#5)

--- src/api/purchase/controllers/purchase.js
+++ src/api/purchase/controllers/purchase.js
@@ -6,4 +6,15 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::purchase.purchase');
+module.exports = createCoreController('api::purchase.purchase', ({ strapi }) => ({
+  async create(ctx) {
+    const { data } = await super.create(ctx);
+    
+    const result = await strapi.db.query('api::purchase.purchase').findOne({
+      where: { id: data.id },
+      populate: ['attendees']
+    });
+
+    return this.transformResponse(result);
+  }
+}));
