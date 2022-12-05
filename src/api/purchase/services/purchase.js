2e241ded8c81b1db3c9bb6809a212fcdd4c40f92
Leandro Alves <leandrosustenido@gmail.com>
2022-12-05 04:29:11 -0300

    fix: Corrige endereco de feedback com hashtag

--- src/api/purchase/services/purchase.js
+++ src/api/purchase/services/purchase.js
@@ -35,9 +35,9 @@ module.exports = createCoreService('api::purchase.purchase', ({ strapi }) => ({
         }
       ],
       back_urls: {
-        "success": `${process.env.FRONT_END_APP_URL}/#/feedback-payment`,
-        "failure": `${process.env.FRONT_END_APP_URL}/#/feedback-payment`,
-        "pending": `${process.env.FRONT_END_APP_URL}/#/feedback-payment`
+        "success": `${process.env.FRONT_END_APP_URL}/feedback-payment`,
+        "failure": `${process.env.FRONT_END_APP_URL}/feedback-payment`,
+        "pending": `${process.env.FRONT_END_APP_URL}/feedback-payment`
       },
       auto_return: "approved",
     };
