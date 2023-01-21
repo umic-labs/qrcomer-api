2c63feee483380a426a8c674372b5183583a3108
Leandro Alves <leandrosustenido@gmail.com>
2023-01-20 23:28:21 -0300

    feat: Adiciona email de notificacao de aguardando pagamento

--- src/api/purchase/content-types/purchase/lifecycles.js
+++ src/api/purchase/content-types/purchase/lifecycles.js
@@ -4,20 +4,31 @@ module.exports = {
   async beforeUpdate(data, model) {
     const { id } = data;
 
-    const PENDING = 'pending'
     const APPROVED = 'approved'
+    const WAITING = 'waiting'
 
     const prevPurchase = await strapi.query('api::purchase.purchase').findOne({ id })
     const nextPurchase = data.params.data
 
-    const isApproving = prevPurchase.status === PENDING
+    const isApproving = prevPurchase.status !== APPROVED
       && nextPurchase.status === APPROVED
+
+    const isWaiting = prevPurchase.status !== WAITING
+      && nextPurchase.status === WAITING
 
     if(isApproving) {
       await strapi.service('api::purchase.purchase')
         .sendConfirmationEmail({
           to: prevPurchase.email,
           preferenceId: prevPurchase.preferenceId,
+        });
+    }
+
+    if(isWaiting) {
+      await strapi.service('api::purchase.purchase')
+        .sendNotificationEmail({
+          to: prevPurchase.email,
+          preference: prevPurchase.preference,
         });
     }
   },
